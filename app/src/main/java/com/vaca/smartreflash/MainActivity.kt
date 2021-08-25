472026b5880a61ee6e1f905c5f1c6bf0c299c62f
李光浩 <liguanghao@viatomtech.com>
2021-08-25 09:24:25 +0800

    ghhgj

--- app/src/main/java/com/vaca/smartreflash/MainActivity.kt
+++ app/src/main/java/com/vaca/smartreflash/MainActivity.kt
@@ -3,8 +3,11 @@ package com.vaca.smartreflash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.scwang.smart.refresh.footer.BallPulseFooter
+import com.scwang.smart.refresh.header.BezierRadarHeader
 import com.scwang.smart.refresh.layout.SmartRefreshLayout
 import com.scwang.smart.refresh.layout.api.RefreshLayout
+import com.scwang.smart.refresh.layout.constant.SpinnerStyle
 import com.scwang.smart.refresh.layout.listener.OnRefreshListener
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val fuck:SmartRefreshLayout=findViewById(R.id.refreshLayout)
+        fuck.setRefreshHeader(com.scwang.smart.refresh.header.MaterialHeader(this));
+        fuck.setRefreshFooter(BallPulseFooter(this).setSpinnerStyle(SpinnerStyle.Scale));
         fuck.setOnRefreshListener(object :OnRefreshListener{
             override fun onRefresh(refreshLayout: RefreshLayout) {
                Log.e("fuck","fuck")
